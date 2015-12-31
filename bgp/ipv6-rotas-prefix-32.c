1eb7ad091d0ce7bba8163e51d5c0ee4db26a8d60
Patrick Brandão <patrickbrandao@gmail.com>
2015-12-30 23:05:42 -0200

    Create ipv6-rotas-prefix-32.c

---
+++ bgp/ipv6-rotas-prefix-32.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(int argc, char **argv){
+    struct in6_addr ipv6;
+     char str[INET6_ADDRSTRLEN];
+    int a = 32;
+    int b = 0;
+    int c = 0;
+    int d = 0;
+    
+    memset(&ipv6, 0, sizeof(struct in6_addr));
+
+    for(;a <= 63;a++){
+		ipv6.s6_addr[0] = a;
+		for(b=0;b<=255;b++){
+			ipv6.s6_addr[1] = b;
+			for(c=0;c<=255;c++){
+			    ipv6.s6_addr[2] = c;
+			    for(d=0;d<=255;d++){
+				ipv6.s6_addr[3] = d;
+				inet_ntop(AF_INET6, &(ipv6), str, INET6_ADDRSTRLEN);
+				printf("%s/32\n", str);
+			    }
+			}
+		}
+    
+    }
+    return 0;
+}
